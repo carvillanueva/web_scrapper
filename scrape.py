547cd4fe4e6e8dda4a6b75096436906802d648df
Carlos Villanueva <carlos2villanueva@gmail.com>
2025-01-05 16:26:55 -0500

    update helper functions

--- scrape.py
+++ scrape.py
@@ -1,21 +1,48 @@
-import selenium.webdriver as webdriver
-from selenium.webdriver.chrome.service import Service
-import time
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+from bs4 import BeautifulSoup
+
+SBR_WEBDRIVER = "http://localhost:4444/wd/hub"
+
 
 def scrape_website(website_url):
     print('Launching browser...')
 
-    chrome_driver_path = "./chromedriver"
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
-
-    try:
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
+        print('Connected! Navigating to https://example.com...')
         driver.get(website_url)
-        print("Page loaded...")
+        # CAPTCHA handling: If you're expecting a CAPTCHA on the target page, use the following code snippet to check the status of Scraping Browser's CAPTCHA solver
+        print('Waiting captcha to solve...')
+        solve_res = driver.execute('executeCdpCommand', {
+            'cmd': 'Captcha.waitForSolve',
+            'params': {
+                'detectTimeout': 10000
+            }
+        })
+        print('Captcha solve status:', solve_res['value']['status'])
+        print('Navigated! Scraping page content...')
         html = driver.page_source
-        time.sleep(10)
+        print(html)
 
-        return html
-    finally:
-        driver.quit()
-        print("Browser closed...")
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, 'html.parser')
+    for script_or_style in soup(['script', 'style']):
+        script_or_style.extract()
+
+    cleaned_content = soup.get_text(separator='\n')
+    # Remove extra characters
+    cleaned_content = "\n".join([line.strip() for line in cleaned_content.splitlines() if line.strip()])
+
+    return cleaned_content
+
+# Split the DOM content into chunks of max_tokens to allow for LLM processing
+def split_dom_content(dom_content, max_tokens=6000):
+    return [dom_content[i:i+max_tokens] for i in range(0, len(dom_content), max_tokens)]
